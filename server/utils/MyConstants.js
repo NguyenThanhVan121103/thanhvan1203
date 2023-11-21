3ec0389f0e1ab33c331ad791acdbba06712d3dea
NguyenThanhVan121103 <127576160+NguyenThanhVan121103@users.noreply.github.com>
2023-11-21 21:53:07 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+
+const MyConstants = {
+  DB_SERVER: 'cluster0.aqcmc5y.mongodb.net',
+  DB_USER: 'KimNhung',
+  DB_PASS: 'N123456',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'TraThiKimNhung',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: 'Thanhvan.12803@gmail.com', // gmail service
+  EMAIL_PASS: 'Vandepzai123'
+};
+module.exports = MyConstants;
